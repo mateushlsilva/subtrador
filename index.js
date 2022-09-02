d9bbb651425fda37a64ddf480a6f53334a49eef5
mateushls <mateushls01@gmail.com>
2022-09-02 10:12:28 -0300

    Adiciona subtrador

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Subtrador {
+    subtrador(numero1, numero2) {
+        return numero1 - numero2;
+    }
+}
+exports.default = Subtrador;
